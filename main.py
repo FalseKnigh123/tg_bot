8873581a212cd8d987e50cfda2c1f1f4c4c26797
FalseKnigh123 <atruppikov@gmail.com>
2023-04-18 10:57:53 +0300

    Готовый шаблон без бд

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 contr = ""
 count = 0
 cor = 0
+info = ''
 
 
 def get_response(countru):
@@ -60,7 +61,15 @@ async def game_capital(update, context):
 
 
 async def game_flag(update, context):
-    await update.message.reply_text(update.message.text)
+    await update.message.reply_text(
+        "Привет! И так начнем игру")
+    info = get_response('us')  # Рандом и бд
+    await update.message.reply_text(
+        f"картинка") # Обработка картинок
+    global count, cor
+    count = 0
+    cor = 0
+    return 1
 
 
 async def first_response_info(update, context):
@@ -89,8 +98,9 @@ async def stop(update, context):
 
 
 async def first_response_capital(update, context):
+    global info
     locality = update.message.text
-    info = get_response('us')
+    info = get_response('us') # генератор случ страны
     global count, cor
     if locality == info['data']['name']:
         await update.message.reply_text('Верно')
@@ -99,15 +109,30 @@ async def first_response_capital(update, context):
         await update.message.reply_text("Неверно")
         count += 1
     if count < 2:
+        await update.message.reply_text("Хотели бы вы подробнее узнать о стране?")
         return 2
     await update.message.reply_text(f'Вы проиграли вас результат {cor}')
     return ConversationHandler.END
 
 
 async def second_response_capital(update, context):
-    info = get_response('us')  # Рандом и бд
+    global info
+    info = get_response('us')  # Рандом и бд  Вместо Us должна быть переменная с кодом страны
+    locality = update.message.text
+    print(locality)
+    if locality == 'да':
+        await update.message.reply_text(
+            f"Полное название страны {info['data']['name']}\n"
+            f"Столица {info['data']['capital']}\n")
+    await update.message.reply_text(f"Продолжаем")
     await update.message.reply_text(
         f"{info['data']['capital']}\n")
+    return 3
+
+
+async def third_response_capital(update, context):
+    global info
+    info = get_response('us') # Рандом и бд  Вместо Us должна быть переменная с кодом страны
     locality = update.message.text
     global count, cor
     if locality == info['data']['name']:
@@ -117,7 +142,57 @@ async def second_response_capital(update, context):
         await update.message.reply_text("Неверно")
         count += 1
     if count < 2:
-        return 1
+        await update.message.reply_text("Хотели бы вы подробнее узнать о стране")
+        return 2
+    await update.message.reply_text(f'Вы проиграли вас результат {cor}')
+    return ConversationHandler.END
+
+
+async def first_response_flag(update, context):
+    locality = update.message.text
+    global info
+    info = get_response('us') # Рандом и бд  Вместо Us должна быть переменная с кодом страны
+    global count, cor
+    if locality == info['data']['name']:
+        await update.message.reply_text('Верно')
+        cor += 1
+    else:
+        await update.message.reply_text("Неверно")
+        count += 1
+    if count < 2:
+        await update.message.reply_text("Хотите ли вы узнать подробнее о стране?")
+        return 2
+    await update.message.reply_text(f'Вы проиграли вас результат {cor}')
+    return ConversationHandler.END
+
+
+async def second_response_flag(update, context):
+    global info
+    info = get_response('us')  # Рандом и бд  Вместо Us должна быть переменная с кодом страны
+    locality = update.message.text
+    if locality == 'да':
+        await update.message.reply_text(
+            f"Полное название страны {info['data']['name']}\n"
+            f"Столица {info['data']['capital']}\n")
+    await update.message.reply_text(f"Продолжаем")
+    await update.message.reply_text(f"Здесь должа быть картинка") # Picrute
+    return 3
+
+
+async def third_response_flag(update, context):
+    global info
+    info = get_response('us')  # Рандом и бд  Вместо Us должна быть переменная с кодом страны
+    locality = update.message.text
+    global count, cor
+    if locality == info['data']['name']:
+        await update.message.reply_text('Верно')
+        cor += 1
+    else:
+        await update.message.reply_text("Неверно")
+        count += 1
+    if count < 2:
+        await update.message.reply_text("Хотели бы вы подробнее узнать о стране")
+        return 2
     await update.message.reply_text(f'Вы проиграли вас результат {cor}')
     return ConversationHandler.END
 
@@ -134,7 +209,18 @@ capital_handler = ConversationHandler(
     entry_points=[CommandHandler('game_capital', game_capital)],
     states={
         1: [MessageHandler(filters.TEXT & ~filters.COMMAND, first_response_capital)],
-        2: [MessageHandler(filters.TEXT & ~filters.COMMAND, second_response_capital)]
+        2: [MessageHandler(filters.TEXT & ~filters.COMMAND, second_response_capital)],
+        3: [MessageHandler(filters.TEXT & ~filters.COMMAND, third_response_capital)]
+    },
+    fallbacks=[CommandHandler('stop', stop)]
+)
+
+flag_handler = ConversationHandler(
+    entry_points=[CommandHandler('game_flag', game_flag)],
+    states={
+        1: [MessageHandler(filters.TEXT & ~filters.COMMAND, first_response_flag)],
+        2: [MessageHandler(filters.TEXT & ~filters.COMMAND, second_response_flag)],
+        3: [MessageHandler(filters.TEXT & ~filters.COMMAND, third_response_flag)]
     },
     fallbacks=[CommandHandler('stop', stop)]
 )
@@ -144,7 +230,7 @@ def main():
     application = Application.builder().token("6211811458:AAE10kS4o9HfzHTQa_uGXHaWXuqu2bia83A").build()
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
-    application.add_handler(CommandHandler("game_flag", game_flag))
+    application.add_handler(flag_handler)
     application.add_handler(info_handler)
     application.add_handler(capital_handler)
     application.run_polling()
