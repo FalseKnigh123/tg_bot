d33dfa8ef14a9a57b500a9195b3f7353f3c7027c
ilyat <ilyatest15@gmail.com>
2023-04-21 23:03:04 +0300

    new bd

--- data/countries.py
+++ data/countries.py
@@ -14,11 +14,21 @@ class Country(SqlAlchemyBase):
     fullname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     english = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     alpha2 = sqlalchemy.Column(sqlalchemy.String, nullable=False)
-    alpha3 = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    iso = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     location = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     location_precise = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    capital = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    currentcy = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    square = sqlalchemy.Column(sqlalchemy.Float, nullable=False)
+    language = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    population = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
 
+
+class Flag(SqlAlchemyBase):
+    __tablename__ = 're'
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.datetime.now)
 
--- main2.py
+++ main2.py
@@ -8,7 +8,7 @@ from data.countries import Country
 
 def loadTable(table_name):
     with open(table_name, encoding="utf8") as csvfile:
-        reader = csv.reader(csvfile, delimiter='\t', quotechar='"')
+        reader = csv.reader(csvfile, delimiter=';', quotechar='"')
         title = next(reader)
         return list(reader)
 
@@ -18,16 +18,19 @@ def create_table():
     db_sess.query(Country).filter(True).delete()
     db_sess.commit()
 
-    for cont in loadTable('www.artlebedev.ru.csv'):
+    for cont in loadTable('for_bd.csv'):
         country = Country()
         country.name = cont[0]
         country.fullname = cont[1]
         country.english = cont[2]
         country.alpha2 = cont[3]
-        country.alpha3 = cont[4]
-        country.iso = cont[5]
-        country.location = cont[6]
-        country.location_precise = cont[7]
+        country.location = cont[4]
+        country.location_precise = cont[5]
+        country.capital = cont[6]
+        country.currentcy = cont[7]
+        country.square = float(cont[8]) if cont[8] else 0
+        country.language = cont[9]
+        country.population = int(cont[10]) if cont[10] else 0
         db_sess.add(country)
     db_sess.commit()
 
